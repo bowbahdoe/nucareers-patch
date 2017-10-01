46f9ce71d47056c6c9d6346bcab20890635a767e
Ethan McCue <emccue@live.com>
2017-09-30 21:24:21 -0400

    Working for one page, needs fix for changing pages

---
+++ run_patch.js
@@ -0,0 +1,89 @@
+var isJobLink = element => {
+  return element.className === '' &&
+    element.children.length === 0 &&
+    element.innerHTML !== "Qualifiers" &&
+    element.innerHTML !== "Overview"
+}
+
+var getAllJobLinks = function() {
+  // The semicolon at the end of javascript:void(0); is VERY important
+  let links = document.querySelectorAll(`[href="javascript:void(0);"]`)
+  links = Array.from(links);
+  links = links.filter(isJobLink)
+  return links
+}
+
+var NUCAREERS_LINK = 'https://nucareers.northeastern.edu/myAccount/co-op/jobs.htm'
+
+var remove_on_click = function(element) {
+  element.setAttribute('onclick', null)
+  let new_element = element.cloneNode(true);
+  element.parentNode.replaceChild(new_element, element);
+}
+
+
+var patchAllJobLinks = function(...arguments) {
+  let jobs = getAllJobLinks()
+  let on_clicks = []
+
+  for(let job of jobs) {
+    on_clicks.push(job.getAttribute('onclick'))
+    remove_on_click(job)
+  }
+
+  jobs = getAllJobLinks()
+  for(let i = 0; i<jobs.length; i++) {
+    let job = jobs[i]
+    let prevOnClick = on_clicks[i]
+    job.onclick = event => {
+      let w = window.open(NUCAREERS_LINK)
+      w.onload = function() {
+        var script = document.createElement('script');
+        script.innerHTML = `${prevOnClick};onclick();`
+        w.document.head.appendChild(script);
+      }
+    }
+  }
+}
+
+patchAllJobLinks();
+
+let MUTATION_OBSERVER = new MutationObserver(patchAllJobLinks)
+let ALL_CHANGES = {
+  childList:true,
+  attributes: true,
+  characterData: true,
+  subtree: true
+}
+
+for(let e of document.getElementsByClassName("orbis-posting-actions")) {
+  new MutationObserver(() => alert("Changed")).observe(e,
+                            ALL_CHANGES)
+}
+
+
+
+/*var getAllNumberButtons = function() {
+  let links = document.getElementsByTagName('a')
+  links = Array.from(links);
+
+  let numbers = new Set()
+  for(let i = 1; i<100; i++) {
+     numbers.add(i.toString())
+  }
+
+  links = links.filter(link => numbers.has(link.innerHTML))
+  return links
+}
+
+var patchNumberButtons = function() {
+  let buttons = getAllNumberButtons();
+  buttons.map(remove_on_click)
+  buttons = getAllNumberButtons();
+
+  for(let button of buttons) {
+    prevOnClick = button.onclick
+    button.setAttribute("onclick",
+      `loadPostingTable('', '',  'Forward', ${button.innerHTML},'myProgramPostingsCount','', patchAllJobLinks)`)
+  }
+}*/
